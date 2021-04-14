39520e7f6259b26d15869a71086f1a123a9b6d28
jjuanramos <jjuanramos91@gmail.com>
2021-04-14 19:09:05 +0200

    added docker-compose

--- fastpy/build_and_serve.sh
+++ fastpy/build_and_serve.sh
@@ -1,3 +1,10 @@
 # export currently used libraries
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 
+# build
+docker-compose build
+
+# serve
+docker-compose up -d
+
+# find the app at http://localhost:8004/ping
