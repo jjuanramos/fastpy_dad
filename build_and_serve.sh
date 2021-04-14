0f74c824ee7ba3190a0a5324b8a917461bae46d9
jjuanramos <jjuanramos91@gmail.com>
2021-04-14 19:11:49 +0200

    moved build_and_serve

--- build_and_serve.sh
+++ build_and_serve.sh
@@ -1,3 +1,8 @@
+# if permission denied try chmod -R 777 ./build_and_serve.sh
+
+# move to fastpy
+cd fastpy
+
 # export currently used libraries
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 
@@ -6,5 +11,6 @@ docker-compose build
 
 # serve
 docker-compose up -d
+
 
 # find the app at http://localhost:8004/ping
